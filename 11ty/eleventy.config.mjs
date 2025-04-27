fabcdf320170ce06f8edbcc435e5f7c41a185526
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2025-04-26 21:29:25 -0400

    Default template config and styles

--- 11ty/eleventy.config.mjs
+++ 11ty/eleventy.config.mjs
@@ -1,12 +1,16 @@
-import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation"
 
 export const config = {
     dir: {
-        input: "src"
-    }
-};
+        input: "src",
+        layouts: "_includes/layouts"
+    },
+    markdownTemplatEngine: "njk"
+}
 
 export default function (eleventyConfig) {
-	eleventyConfig.addPlugin(eleventyNavigationPlugin);
-};
+	eleventyConfig.addPlugin(eleventyNavigationPlugin)
+    eleventyConfig.addPassthroughCopy("./src/styles.css")
+    eleventyConfig.addGlobalData("layout", "base.njk")
+}
 
